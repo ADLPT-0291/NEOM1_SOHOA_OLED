885509be347f89b9d7d53f79816f631ed54b69b6
phamdung1211 <phamngocdung1211@gmail.com>
2024-11-13 19:51:11 +0700

    rrr

--- h3.py
+++ h3.py
@@ -545,7 +545,8 @@ def convert_seconds_to_hhmmss(total_seconds):
     hours = total_seconds // 3600
     minutes = (total_seconds % 3600) // 60
     seconds = total_seconds % 60
-    return f"{int(hours):02d}:{int(minutes):02d}:{int(seconds):02d}"
+    return "{:02d}:{:02d}:{:02d}".format(int(hours), int(minutes), int(seconds))
+
 
 def job_PhatBanTinTinh(ban_tin, status):
     global DanhSachBanTinDung, IdLichDangPhat, BanTinKeTiep, ChoPhatBanTinTinh  
